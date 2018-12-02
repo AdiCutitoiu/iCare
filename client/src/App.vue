fcf71c69cc7f17c7ff3568ae8a9c3c1872ca71c0
Adi Cutitoiu <acutitoiu2@gmail.com>
2018-12-02 16:53:58 +0200

    Conditionally render based on user login

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,41 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <div v-if="loggedIn">
+      <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase">
+          <span>Vuetify</span>
+          <span class="font-weight-light">MATERIAL DESIGN</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+          <span class="mr-2">Latest Release</span>
+        </v-btn>
+      </v-toolbar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+      <v-content>
+        <HelloWorld/>
+      </v-content>
+    </div>
+    <div v-else>
+      <v-content>
+        <HelloWorld/>
+      </v-content>
+    </div>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
-      //
-    }
+      loggedIn: false
+    };
   }
-}
+};
 </script>
