5864fbc94121dbb493e281131933f53ec90b50ff
Adi Cutitoiu <acutitoiu2@gmail.com>
2019-01-01 23:21:10 +0200

    Add authentication router

---
+++ server/src/routers/authentication.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+router.post('/register', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+module.exports = router;
