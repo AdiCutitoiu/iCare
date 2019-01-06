b0419cbf7c1829e5dd21337eb636757271d0c952
qcataa <catalin.mcm13@gmail.com>
2019-01-06 11:23:39 +0200

    Add Card Grid and Card Layout

---
+++ client/src/components/CardsGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <Card></Card>
+  </v-container>
+
+</template>
+
+<script>
+import Card from "./Card";
+export default {
+  name: "CardsGrid",
+  components: {
+    Card
+  },
+  data: () => ({})
+};
+</script>
+
+<style>
+</style>
--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -3,20 +3,22 @@
     <v-layout text-xs-center wrap>
       <v-flex xs12></v-flex>
     </v-layout>
-    
+
         <Sidebar @signout="$emit('signout')"/>
-        
+        <CardsGrid></CardsGrid>
   </v-container>
 
 </template>
 
 <script>
 import Sidebar from "./Sidebar";
+import CardsGrid from "./CardsGrid";
 
 export default {
   name: "Dashboard",
   components: {
-    Sidebar
+    Sidebar,
+    CardsGrid
   },
   data: () => ({})
 };
