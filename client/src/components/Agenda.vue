9f0736f104935e64281bc59ae5e9d35ec3b5ce48
qcataa <catalin.mcm13@gmail.com>
2019-01-07 22:50:08 +0200

    Add agenda functionality Remove Card functionality Renamed CardsGrid to Agenda.vue

---
+++ client/src/components/Agenda.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+      <kalendar :configuration="calendar_settings" :appointments="appointments"/>
+  </v-container>
+
+</template>
+
+<script>
+import { Kalendar } from 'kalendar-vue';
+import 'kalendar-vue/dist/KalendarVue.css';
+
+export default {
+  name: "Agenda",
+  components: {
+    Kalendar
+  },
+  data: () => ({
+    appointments: [],
+	  calendar_settings: {
+		style: 'material_design', 
+		view_type: 'Month', 
+		split_value: 15, 
+		cell_height: 20, 
+		scrollToNow: true,
+		current_day: new Date(), 
+	},
+  })
+};
+</script>
+
+<style>
+</style>
